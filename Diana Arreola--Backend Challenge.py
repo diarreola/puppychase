9c74295238da95c90f4284c946508ddd4c81a54e
Diana Arreola <arreoladiana11@gmail.com>
2020-01-24 17:55:43 -0800

    added new files

--- Diana Arreola--Backend Challenge.py
+++ Diana Arreola--Backend Challenge.py
@@ -1,4 +1,32 @@
 # Codecademy Backend Challenge
 # CREATED BY: Diana Arreola
 # DESCRIPTION: Model that gives predictions on whether an advertisement has the
+import random
+import os
+import ufo as ufo
+
+
+# create tests for random noun
+def randomizeNoun():
+    """ randomeizeNoun returns a random noun from the
+        nouns.txt file 
+    """
+    with open("nouns.txt") as noun_file:
+        nouns = noun_file.read().split()
+    random_noun = random.choice(nouns)
+    return random_noun
+
+def UFOGame():
+    """ randomeizeNoun returns a random noun from the
+        nouns.txt file 
+    """
+    codeword = randomizeNoun()
+
+    #  initial showcase
+    print("UFO: The Game")
+    print("Instructions: save us from alien abduction by guessing letters in the codeword.")
+    print(ufo.x[0])
+
+UFOGame()
+
 
