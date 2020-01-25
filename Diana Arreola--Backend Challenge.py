5bd6908b8f5604208c0813f6fad4571113eff96b
Diana Arreola <arreoladiana11@gmail.com>
2020-01-24 21:12:05 -0800

    debugging

--- Diana Arreola--Backend Challenge.py
+++ Diana Arreola--Backend Challenge.py
@@ -49,13 +49,13 @@ def validAnswer(dashed_codeword):
 def playAgain(user_input):
     if (user_input == "N") | (user_input == "n") | (user_input == "No") | (user_input == "NO")| (user_input == "no"):
         print('Goodbye')
-        return None
+        return 0
     elif (user_input == "Y") | (user_input == "y") | (user_input == "Yes") | (user_input == "yes") | (user_input == "YES"):
         print('Lets play again')
         return UFOGame()
     else:
         print('I did not understand you. Bye!')
-        return None
+        return 0
 
 def checkInput(user_input):
 
@@ -71,7 +71,7 @@ def incorrectGuess(chances_left, codeword, incorrect_guesses, guessed_codeword):
 
     # lost game
     if chances_left == 0:
-        lostGame(codeword)
+        return lostGame(codeword)
 
     # incorrect guess
     print("Incorrect! The tractor beam pulls the person in further.")
@@ -83,13 +83,13 @@ def incorrectGuess(chances_left, codeword, incorrect_guesses, guessed_codeword):
     letter_guessed = input("Please enter your guess: ")
     letter_guessed = checkInput(letter_guessed)
     letter_guessed = letter_guessed.upper()
-    return chances_left
+    return chances_left, letter_guessed
 
 def lostGame(word):
     print("Incorrect! The tractor beam pulls you all the way in. You Lost.")
     print("The codeword is: ", word, ".")
     answer = input("Would you like to play again (Y/N)?")
-    playAgain(answer)
+    return playAgain(answer)
 
 def wonGame(word):
     print("Correct! You saved the person and earned a medal of honor!")
@@ -141,49 +141,14 @@ def UFOGame():
                 
         elif letter_guessed in incorrect_guesses:
             print("Already Guessed That!")
-            # chances_left -= 1
-
-            # # lost game
-            # if chances_left == 0:
-            #     lostGame(codeword)
-
-            # # incorrect guess
-            # print("Incorrect! The tractor beam pulls the person in further.")
-            # print(ufo.x[abs(chances_left-6)])
-            # print("Already Guessed That!")
-            # print("Incorrect guesses: ")
-            # print(' '.join(letter for letter in incorrect_guesses))
-            # print("Codeword: ")
-            # print(guessed_codeword)
-            # letter_guessed = input("Please enter your guess: ")
-            # letter_guessed = checkInput(letter_guessed)
-            # letter_guessed = letter_guessed.upper()
-            chances_left = incorrectGuess(chances_left, codeword, incorrect_guesses, guessed_codeword)
+            chances_left,letter_guessed = incorrectGuess(chances_left, codeword, incorrect_guesses, guessed_codeword)
 
         elif letter_guessed not in codeword:
             incorrect_guesses.append(letter_guessed)
-            chances_left = incorrectGuess(chances_left, codeword, incorrect_guesses, guessed_codeword)
-            # chances_left -= 1
-
-            # # lost game
-            # if chances_left == 0:
-            #     lostGame(codeword)
-
-            # # incorrect guess
-            # print("Incorrect! The tractor beam pulls the person in further.")
-            # print(ufo.x[abs(chances_left-6)])
-            # print("Incorrect guesses: ")
-            # print(' '.join(letter for letter in incorrect_guesses))
-            # print("Codeword: ")
-            # print(guessed_codeword)
-            # letter_guessed = input("Please enter your guess: ")
-            # letter_guessed = checkInput(letter_guessed)
-            # letter_guessed = letter_guessed.upper()
-
+            chances_left,letter_guessed = incorrectGuess(chances_left, codeword, incorrect_guesses, guessed_codeword)
+            
             
     
-    
-
 UFOGame()
 
 
